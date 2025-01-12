5d442a7a43fd265942f1d0f478caede99ca7d9f8
saket07singh <saket07singh@gmail.com>
2025-01-12 21:23:16 +0530

    modified

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("hello")
+import streamlit as st
+
+def main():
+    st.title("Hello, Streamlit!")
+    st.write("This is a simple hello world page.")
+
+if __name__ == "__main__":
+    main()
